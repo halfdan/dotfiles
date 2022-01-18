1304cd5e1c39bbb0d1814a896ed3f245bd502f6d
Fabian Becker <halfdan@xnorfz.de>
2022-01-18 08:42:25 +0100

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,21 +7,27 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Install asdf-vm
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 
-# Install pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# Install asdf plugins
+asdf plugin add python
+asdf plugin add golang
+
+# Install rustup toolchain
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # Link files
-ln -s ~/.dotfiles/.zshrc .zshrc
-ln -s ~/.dotfiles/.zshenv .zshenv
-ln -s ~/.dotfiles/.tool-versions .tool-versions
-ln -s ~/.dotfiles/.tmux.conf .tmux.conf
-ln -s ~/.dotfiles/.vim .vim
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
+ln -s ~/.dotfiles/.tool-versions ~/.tool-versions
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/.config/i3status ~/.config/i3status
+ln -s ~/.dotfiles/.config/alacritty ~/.config/alacritty
 
 # Source zsh
 source ~/.zshrc
