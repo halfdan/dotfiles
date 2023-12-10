33031165b5186e68fa2846f5be2ec5641b3a435b
Fabian Becker <fbecker@adobe.com>
2023-12-10 16:16:47 +0100

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,11 @@
 local wezterm = require"wezterm"
+local mux = wezterm.mux
+local act = wezterm.action
+
+wezterm.on('gui-startup', function()
+ local tab, pane, window = mux.spawn_window({})
+ window:gui_window():maximize()
+end)
 
 return {
   -- Font settings
@@ -12,8 +19,21 @@ return {
   -- Tab bar
   show_tab_index_in_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
   -- Window look
   window_decorations = "RESIZE",
   -- Wayland
   enable_wayland = true,
+  inactive_pane_hsb = {
+   saturation = 0.8,
+   brightness = 0.7
+  },
+  window_frame = {
+   font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
+  },
+  scrollback_lines = 26000,
+  -- Keybindings
+  -- disable_default_key_bindings = true,
+
+  leader = { key = 'b', mods = 'CMD', timeout_milliseconds = 2000 },
 }
